61c2ff4dac8447bf6160088543873cdab786fbfc
Paul Park <ppark482@gmail.com>
2015-05-31 20:58:36 -0400

    added echonest credentials and service

---
+++ app/shared/EchoNestService.js
@@ -0,0 +1,27 @@
+;(function(){
+
+	angular.module('ftNotMApp')
+		.factory('EchoNest', EchoNestFactory);
+
+	EchoNestFactory.$inject = ['$q', '$scope', '$http'];
+
+	function EchoNestFactory ($q, $scope, $http) {
+
+		var settings = {
+			apiBase : 'http://developer.echonest.com/api/v4/',
+			apiKey : 'XLDPJFLJAI1Z6MJN0',
+			consumerKey : 'c6a66194df1522e149ffe383e538baf0',
+			sharedSecret : 'wb3ashGcRHGZdjgd7PCjng'
+		};
+
+		function getSettings () {
+			return settings;
+		}
+
+		return {
+			getSettings : getSettings
+		}
+
+	}; // end echo nest factory
+
+}());
--- app/shared/SpotifyService.js
+++ app/shared/SpotifyService.js
@@ -1,4 +1,4 @@
-(function(){
+;(function(){
 
 	angular.module('ftNotMApp')
 		.factory('Spotify', SpotifyFactory);
